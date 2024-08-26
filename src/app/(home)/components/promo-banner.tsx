b24444a5b729f436617797dc999b2f750cd01d4a
Ryan <ryan.figueredo059@gmail.com>
2024-08-25 22:38:38 -0300

    feat: add Promo Banner and SectionTitle component correct

--- src/app/(home)/components/promo-banner.tsx
+++ src/app/(home)/components/promo-banner.tsx
@@ -5,7 +5,7 @@ const PromoBanner = ({ alt, ...props }: ImageProps) => {
     <Image
       height={0}
       width={0}
-      className="mt-8 h-auto w-full px-5"
+      className="h-auto w-full px-5"
       sizes="100vw"
       alt={alt}
       {...props}
