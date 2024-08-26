b6133363df3a60f394cfa150a3b23172064958bf
Ryan <ryan.figueredo059@gmail.com>
2024-08-25 22:47:14 -0300

    feat: add footer

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <div className="bg-accent px-8 py-4">
+      <p className="text-center text-[0.625rem] text-white">
+        © 2024, <span className="font-semibold">Ryan Figueredo</span> Todos os
+        direitos reservados.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
