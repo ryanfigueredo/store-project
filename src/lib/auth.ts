2cf0ff0b7a43068feee789d86246a26a67a7ec8c
Ryan <ryan.figueredo059@gmail.com>
2024-08-27 19:24:57 -0300

    chore: prepare add script prisma generate fix

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,4 @@
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
 import { AuthOptions } from "next-auth";
 import { prismaClient } from "./prisma";
